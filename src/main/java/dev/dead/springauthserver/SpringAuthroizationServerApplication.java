a83265053564f83a214c835e5b9b1a64bec8be07
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-25 18:24:44 +0300

    init

---
+++ src/main/java/dev/dead/springauthserver/SpringAuthroizationServerApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.springauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthroizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthroizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dead/springauthserver/SpringAuthroizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.springauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthroizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
